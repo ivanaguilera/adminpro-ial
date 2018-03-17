6c94b5efb67fa0426f9894a9c2e3f56407388311
new-pc\Iván Aguilera <aguileraian2@gmail.com>
2018-03-17 15:34:07 -0300

    Primer Commit

---
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrums',
+  templateUrl: './breadcrumbs.component.html',
+  styles: []
+})
+export class BreadcrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
